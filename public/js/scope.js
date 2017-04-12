861efd77abef2280aabd1d2185b8b10567c87152
Chris Oakman <chris.oakman@gmail.com>
2017-04-12 18:30:12 -0500

    saving state

--- public/js/scope.js
+++ public/js/scope.js
@@ -1,24 +1,28 @@
-'use strict'
 
-var favFruit = 'banana'
 
-doAThing()
 
-// What is the value of favFruit?
-console.log(favFruit)
-
-function doAThing () {
-  insideAThing()
-
-  function insideAThing () {
-    favFruit = 'orange'
-  }
+function Popup (el, html) {
+  var popup = {}
+  popup.show = function () { }
+  popup.hide = function () { }
+  popup.setHtml = function (newHtml) { }
+  return popup
 }
 
-if (favFruit === 'banana') {
-  let myName = 'Chris'
-  console.log(myName)
+Popup.prototype.isShowing = false
+
+var menu1 = Popup(byId('btn1'), '<h1>I am menu 1!</h1>')
+var menu2 = new Popup(byId('btn2'), '<h2>Menu 2</h2>')
+
+console.log(menu1)
+console.log(menu2)
+
+if (menu1.isShowing) {
+  // do something..
 } else {
-  let yourName = 'Mike'
-  console.log(yourName)
+  // do another thing
+}
+
+function byId (id) {
+  return document.getElementById(id)
 }
