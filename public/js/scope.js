bc4b38250faf070de9b8d063f8fda0fba7f4cf96
Chris Oakman <chris.oakman@gmail.com>
2017-04-13 20:27:19 -0500

    fixing some stuff

--- public/js/scope.js
+++ public/js/scope.js
@@ -11,8 +11,8 @@ function Popup (el, html) {
 
 Popup.prototype.isShowing = false
 
-var menu1 = Popup(byId('btn1'), '<h1>I am menu 1!</h1>')
-var menu2 = new Popup(byId('btn2'), '<h2>Menu 2</h2>')
+var menu1 = Popup(byId('btn2'), '<h1>I am menu 1!</h1>')
+var menu2 = new Popup(byId('btn1'), '<h2>Menu 2</h2>')
 
 console.log(menu1)
 console.log(menu2)
